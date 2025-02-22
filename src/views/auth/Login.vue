97332bf0120a87351b442900c61f86f1a0422a02
chapaco2023 <edwinmarcelofarfan2022@gmail.com>
2025-02-22 17:18:32 -0400

    errores validaciones

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -5,9 +5,11 @@
   <form @submit.prevent="funIngresar()">
     <label for="e">Ingrese su correo</label>
     <input type="email" id="e" required v-model="usuario.email" />
+    {{ errors.email }}
     <br />
     <label for="pass">Ingrese su contraseña</label>
     <input type="password" id="pass" required v-model="usuario.password" />
+    {{ errors.password }}
     <br />
     <input type="submit" value="Ingresar" />
     <a href="">¿Olvidaste tu Contraseña?</a>
@@ -20,8 +22,8 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
-
 const usuario = ref({ email: "", password: "" });
+const errors = ref({});
 
 async function funIngresar() {
   try {
@@ -30,7 +32,12 @@ async function funIngresar() {
     localStorage.setItem("access_token", data.access_token);
     router.push("/servicios");
   } catch (error) {
-    alert("Credenciales Incorrectas");
+    console.log(error.response.data);
+    if (error.response.data.errors) {
+      errors.value = error.response.data.errors;
+    } else {
+      alert("Credenciales Incorrectas");
+    }
   }
 }
 </script>
